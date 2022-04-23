5862b07a08370965d0f68e26ee32f77c9c5ed21d
MURIUKI2092 <muriukijames33@gmail.com>
2022-04-23 06:31:32 +0300

    added a landing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,19 @@
 import { Navbar } from "./components/navbar";
 import "./styles/navbar.css";
-import "./styles/footer.css"
+import "./styles/footer.css";
+import "./styles/header.css";
+import "./styles/inspiration.css";
 import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+import { Inspiration } from "./components/inspiration";
 
 
 function App() {
   return (
    <>
    <Navbar/>
+   <Header/>
+   <Inspiration/>
    <Footer/>
    </>
   );
---
+++ my-app/src/components/experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Experience = () => {
+  return (
+    <>
+    <div className='experience'>
+
+</div>
+    </>
+    
+  )
+}
