a7c76dc2635c4e4595bff297bf99cdb73b86304e
MURIUKI2092 <muriukijames33@gmail.com>
2022-05-19 11:18:08 +0300

    fixed #0007

--- my-app/src/components/host.jsx
+++ my-app/src/components/host.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,121 +12,156 @@ import Container from '@mui/material/Container';
 
 
 export const Host = () => {
-  return (
-    <Grid container component="main" sx={{ height: '90vh' }}  className="post">
-        
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            maxWidth:"400",
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+  
+   
+      const [ username,setUsername]= useState("");
+      const [email,setEmail]= useState("");
+      const [telephone,setTelephone]=useState("");
+      const [location,setLocation]= useState("");
+      const[price,setPrice]= useState("");
+      const[ description,setDescription]= useState("");
+      const[file,setFile]= useState(null);
 
-       <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          
-          
-          <Box component="form"  sx={{ mt: 1 }}>
+      const handleSubmit = async(event)=>{
+        event.preventDefault();
+        const newHost ={
+          username,
+          email,
+          telephone,
+          location,
+          price,
+          description,
 
-            <Typography gutterBottom variant="h5" component="div" className='headerSearch'>
-                
-                Fill to Post
-                    </Typography>
-          <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Username"
-              name="name"
-              autoComplete="namel"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="telephone"
-              label="Telephone"
-               id="password"
-              
-            />
+        }
+        if(file){
+          const data = new FormData();
+          const filename = Date.now()+file.name;
+          data.append("name",filename);
+          data.append("file",file);
+          newHost.photo = filename
+        }
+      }
+      return (
+        <Grid container component="main" sx={{ height: '70vh' }}  className="post">
+            
+            {/* <CssBaseline />
+            <Grid
+              item
+              xs={false}
+              sm={4}
+              md={7}
+              sx={{
+                maxWidth:"400",
+                backgroundImage: 'url(https://source.unsplash.com/random)',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: (t) =>
+                  t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            /> */}
+            <Grid item xs={12} sm={18} md={10} component={Paper} elevation={6} square>
 
-
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="location"
-              label="Host location"
-              name="location"
-              autoComplete="location"
-              
-            />
-               <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="money"
-              label="Price"
-              name="Price"
-              autoComplete="Price"
-              
-            />
-             <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="description"
-              label="description"
-              name="description"
-              autoComplete="description"
-              
-            />
-            <input type={"file"} className="upload" />
-                          
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
             >
-            Post
-            </Button>
-            </Box>
-             </Box>
-            </Container>
+              
+              
+              <Box component="form"  sx={{ mt: 1 }} >
 
-            </Grid>
-      </Grid>
-  )
+                <Typography gutterBottom variant="h5" component="div" className='headerSearch'>
+                    
+                    Fill to Post
+                        </Typography>
+              <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Username"
+                  name="name"
+                  autoComplete="namel"
+                  autoFocus
+                  onChange={setUsername}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={setEmail}
+                  
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="telephone"
+                  label="Telephone"
+                  id="telephone"
+                  onChange={setTelephone}
+                />
+
+
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="location"
+                  label="Host location"
+                  name="location"
+                  autoComplete="location"
+                  onChange={setLocation}
+                  
+                />
+                  <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="money"
+                  label="Price"
+                  name="Price"
+                  autoComplete="Price"
+                  onChange={setPrice}
+                  
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="description"
+                  label="description"
+                  name="description"
+                  autoComplete="description"
+                  onChange={setDescription}
+                  
+                />
+                <input type={"file"} className="upload" onChange={(e)=>setFile(e.target.files[0])} />
+                              
+                <Button
+                onSubmit={handleSubmit}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                Post
+                </Button>
+                </Box>
+                </Box>
+                </Container>
+
+                </Grid>
+          </Grid>
+      )
 };
